b5558de2f81dc9bd4fb2b5386962717a9c61b00e
Leslie <s1365767@monmouth.edu>
2026-01-11 18:43:48 -0500

    fix: remove prisma adapter in backend db

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,5 @@
 // src/db.js
 import "dotenv/config";
-import pkg from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "@prisma/client";
 
-const { PrismaClient } = pkg;
-
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.SQLITE_URL,
-});
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
