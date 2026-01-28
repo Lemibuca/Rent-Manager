20489328be645d63647e93899370d30717852f52
Leslie Bustamante <lesmileydi@gmail.com>
2026-01-28 17:02:23 -0500

    Fix input text color visibility

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.7shf959kteg"
+    "revision": "0.g42eurgeq1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
