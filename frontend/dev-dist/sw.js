bd82d72402da969ac62c26151628f6306dbb3ca0
Leslie <s1365767@monmouth.edu>
2026-01-28 16:16:45 -0500

    Fix input text color visibility in light and dark mode

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.a70v1pp3glo"
+    "revision": "0.7shf959kteg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
